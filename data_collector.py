dc623e2d0a2900bd57e5dfa3e972526f5316b62b
rancheng <chengran327@gmail.com>
2019-06-23 08:29:09 -0400

    update visualization and preload function

--- data_collector.py
+++ data_collector.py
@@ -6,3 +6,61 @@ import pandas as pd
 import time
 import math
 import airsim
+
+
+def init_road_points():
+    road_points = []
+    with open('./Shared/dump_road_lines.txt', 'r') as f:
+        for line in f:
+            points = line.split('\t')
+            first_point = np.array([float(p) for p in points[0].split(',')] + [0])
+            second_point = np.array([float(p) for p in points[1].split(',')] + [0])
+            road_points.append(tuple((first_point, second_point)))
+    return road_points
+
+road_points = init_road_points()
+
+# airsim related
+car_client = airsim.VehicleClient()
+car_client.confirmConnection()
+car_client.enableApiControl(True)
+car_controls = airsim.CarControls()
+# Get the current state of the vehicle
+car_state = car_client.getCarState()
+
+def get_random_pose():
+    # Pick a random road.
+    random_line_index = np.random.randint(0, high=len(road_points))
+
+    # Pick a random position on the road.
+    # Do not start too close to either end, as the car may crash during the initial run.
+    random_interp = (np.random.random_sample() * 0.4) + 0.3
+
+    # Pick a random direction to face
+    random_direction_interp = np.random.random_sample()
+
+    # Compute the starting point of the car
+    random_line = road_points[random_line_index]
+    random_start_point = list(random_line[0])
+    random_start_point[0] += (random_line[1][0] - random_line[0][0]) * random_interp
+    random_start_point[1] += (random_line[1][1] - random_line[0][1]) * random_interp
+
+    # Compute the direction that the vehicle will face
+    # Vertical line
+    if (np.isclose(random_line[0][1], random_line[1][1])):
+        if (random_direction_interp > 0.5):
+            random_direction = (0, 0, 0)
+        else:
+            random_direction = (0, 0, math.pi)
+    # Horizontal line
+    elif (np.isclose(random_line[0][0], random_line[1][0])):
+        if (random_direction_interp > 0.5):
+            random_direction = (0, 0, math.pi / 2)
+        else:
+            random_direction = (0, 0, -1.0 * math.pi / 2)
+
+    # The z coordinate is always zero
+    random_start_point[2] = 0.01
+    return (random_start_point, random_direction)
+
+starting_points, starting_direction = get_random_pose()
--- map_render.py
+++ map_render.py
@@ -6,8 +6,11 @@ import pandas as pd
 import time
 import math
 import airsim
+from tqdm import tqdm
 
 mpl.style.use('seaborn')
+
+
 # plot the DQN location
 # z = 0
 # pts = [np.array([0, -1, z]), np.array([130, -1, z]), np.array([130, 125, z]), np.array([0, 125, z]),
@@ -38,21 +41,23 @@ def even_select(N, M):
     :param M: number of sample size
     :return: index mask of samples
     '''
-    if M > N/2:
+    if M > N / 2:
         cut = np.zeros(N, dtype=int)
-        q, r = divmod(N, N-M)
-        indices = [q*i + min(i, r) for i in range(N-M)]
+        q, r = divmod(N, N - M)
+        indices = [q * i + min(i, r) for i in range(N - M)]
         cut[indices] = True
     else:
         cut = np.ones(N, dtype=int)
         q, r = divmod(N, M)
-        indices = [q*i + min(i, r) for i in range(M)]
+        indices = [q * i + min(i, r) for i in range(M)]
         cut[indices] = False
 
     return cut
 
+
 def even_select2(len_arr, num_sample):
     idx = np.round(np.linspace(0, len_arr - 1, num_sample)).astype(int)
+
 
 # idx = np.round(np.linspace(0, len(x2) - 1, 6)).astype(int)
 #
@@ -82,6 +87,7 @@ def init_reward_points():
 
     return road_points
 
+
 def init_road_points():
     road_points = []
     with open('./Shared/dump_road_lines.txt', 'r') as f:
@@ -101,13 +107,12 @@ def map_distance(car_point):
     distance = 999
     DISTANCE_DECAY_RATE = 1.2
     for line in reward_points:
-        local_distance = np.linalg.norm(np.cross(line[1] - line[0], line[1] - car_point))/np.linalg.norm(line[1] - line[0])
+        local_distance = np.linalg.norm(np.cross(line[1] - line[0], line[1] - car_point)) / np.linalg.norm(
+            line[1] - line[0])
         distance = min(local_distance, distance)
 
     distance_reward = math.exp(-(distance * DISTANCE_DECAY_RATE))
     return distance_reward
-
-
 
 
 def map_reward(car_point):
@@ -126,6 +131,7 @@ def map_reward(car_point):
     distance_reward = math.exp(-(distance * DISTANCE_DECAY_RATE))
     return distance_reward
 
+
 # Draws the car location plot
 def draw_rl_debug(car_state, road_points):
     # fig = plt.figure(figsize=(15, 15))
@@ -143,19 +149,34 @@ def draw_rl_debug(car_state, road_points):
     plt.plot([car_point[0]], [car_point[1]], 'bo')
     plt.draw()
 
+
 def preload_reward_map():
-    x_dots = np.arange(-135, 135, 1)
-    y_dots = np.arange(-135, 135, 1)
+    x_dots = np.arange(-135, 135, 0.1)
+    y_dots = np.arange(-135, 135, 0.1)
     reward_map_data = np.zeros((len(x_dots), len(y_dots)))
-    for i in range(len(x_dots)):
+    for i in tqdm(range(len(x_dots))):
         for j in range(len(y_dots)):
             sim_car_point = [x_dots[i], y_dots[j], 0]
             reward_map_data[i, j] = map_reward(sim_car_point)
+    reward_map_data.dump('reward_map_data.dat')
+
+
+def quaternion_to_euler(x, y, z, w):
+
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll = math.atan2(t0, t1)
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch = math.asin(t2)
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw = math.atan2(t3, t4)
+    return [yaw, pitch, roll]
 
 
 def test_airsim():
-
-
     car_client = airsim.CarClient()
     car_client.confirmConnection()
     reward_points = init_reward_points()
@@ -165,32 +186,40 @@ def test_airsim():
     for point in reward_points:
         ax.plot([point[0][0], point[1][0]], [point[0][1], point[1][1]], 'k-', lw=2)
 
-    x_dots = np.arange(-130, 130, 0.1)
-    y_dots = np.arange(-130, 130, 0.1)
-    reward_map_data = np.zeros((len(x_dots), len(y_dots)))
-    for i in range(len(x_dots)):
-        for j in range(len(y_dots)):
-            sim_car_point = [x_dots[i], y_dots[j], 0]
-            reward_map_data[i, j] = map_reward(sim_car_point)
+    # x_dots = np.arange(-130, 130, 0.1)
+    # y_dots = np.arange(-130, 130, 0.1)
+    # reward_map_data = np.zeros((len(x_dots), len(y_dots)))
+    # for i in range(len(x_dots)):
+    #     for j in range(len(y_dots)):
+    #         sim_car_point = [x_dots[i], y_dots[j], 0]
+    #         reward_map_data[i, j] = map_reward(sim_car_point)
+    #
+    # ax.plot(reward_map_data)
 
-    ax.plot(reward_map_data)
-
-    position_key = bytes('position', encoding='utf8')
-    x_val_key = bytes('x_val', encoding='utf8')
-    y_val_key = bytes('y_val', encoding='utf8')
     pd = car_client.getCarState().kinematics_estimated.position
     car_point = np.array([pd.x_val, pd.y_val, pd.z_val])
-    p1, = ax.plot([car_point[0]], [car_point[1]], 'bo')
+    arrow0 = arrow_fig([0,0], 0, 8)
+    p1, = ax.plot(arrow0[0], arrow0[1], 'c-')
+    # p1, = ax.plot([car_point[0]], [car_point[1]], 'bo')
     plt.show()
 
     with open("./Shared/log_car_pos.txt", 'w') as f:
         while not car_client.simGetCollisionInfo().has_collided:
             pd = car_client.getCarState().kinematics_estimated.position
+            heading = car_client.getCarState().kinematics_estimated.orientation
+            xyzw = heading.to_numpy_array()
+            ypr = quaternion_to_euler(xyzw[0], xyzw[1], xyzw[2], xyzw[3])
+            yaw = ypr[0]
             car_point = np.array([pd.x_val, pd.y_val, pd.z_val])
+            # arrow plot of position
+            arrow_i = arrow_fig([car_point[0], car_point[1]], yaw, 8)
+            p1.set_xdata(arrow_i[0])
+            p1.set_ydata(arrow_i[1])
             print(map_reward(car_point))
-            p1.set_xdata(car_point[0])
-            p1.set_ydata(car_point[1])
-            p2, = ax.plot([car_point[0]], [car_point[1]], 'r.')
+            # dot plot of position
+            # p1.set_xdata(car_point[0])
+            # p1.set_ydata(car_point[1])
+            # p2, = ax.plot([car_point[0]], [car_point[1]], 'r.')
             fig.canvas.draw()
             fig.canvas.flush_events()
     dummy_carpoint = [14.69277668 - 3.01696157 - 0.5962258]
@@ -211,7 +240,7 @@ def calculate_distance():
     ax2 = fig.add_subplot(122)
     plt.show()
     lsize = 20
-    dislist =  []
+    dislist = []
     mydislist = []
     rewardlist = []
     while not car_client.simGetCollisionInfo().has_collided:
@@ -253,3 +282,13 @@ def calculate_distance():
         fig.canvas.flush_events()
         # print("Distance: %f " %distance)
         # print("My Distance: %f" %map_distance(car_point))
+
+
+def arrow_fig(pos, theta, scale=1):
+    return [[pos[0], pos[0] + scale * np.cos(theta + 5 * np.pi / 4), pos[0] + scale * np.sqrt(2) * np.cos(theta),
+             pos[0] + scale * np.cos(theta + 3 * np.pi / 4), pos[0]],
+            [pos[1], pos[1] + scale * np.sin(theta + 5 * np.pi / 4), pos[1] + scale * np.sqrt(2) * np.sin(theta),
+             pos[1] + scale * np.sin(theta + 3 * np.pi / 4), pos[1]]]
+
+
+# preload_reward_map()
